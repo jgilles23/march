d2798adbc952a26c8dc3f0d48f9673c2414f2ea7
jgilles23 <iisjg23@gmail.com>
2023-03-13 17:07:14 -0700

    Broken, but need file access

---
+++ out/builder2.js
@@ -0,0 +1,91 @@
+console.log("hello world");
+// DEFINE BEFORE AND AFTER FOR GAMES
+const divisor = [0, 2, 4, 8, 16, 32, 64];
+const base = [0, 1, 33, 49, 57, 61, 63];
+const gameToRound = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 6];
+function getGame(team, round) {
+    // Get tournament game number give team number and round number
+    if (team <= 0 || team > 64) {
+        throw "Team number outside of expected bounds";
+    }
+    return Math.floor((team - 1) / divisor[round] + base[round]);
+}
+function getRound(game) {
+    // Get tournamento round number given game number
+    return gameToRound[game];
+}
+function getFirstTeam(game) {
+    //Get first team in a game
+    const round = getRound(game);
+    return (game - base[round]) * divisor[round] + 1;
+}
+function getTeams(game) {
+    //Get all teams in a game
+    let teams = [];
+    let firstTeam = getFirstTeam(game);
+    for (let t = firstTeam; t < firstTeam + divisor[gameToRound[game]]; t++) {
+        teams.push(t);
+    }
+    return teams;
+}
+function nextGame(game) {
+    return getGame(getFirstTeam(game), getRound(game) + 1);
+}
+function previousGames(game) {
+    if (game < base[2]) {
+        return [NaN, NaN];
+    }
+    let firstGame = getGame(getFirstTeam(game), getRound(game) - 1);
+    return [firstGame, firstGame + 1];
+}
+function csvToArray(str, delimiter = ",") {
+    //Take csv as a string and convert to array of Objects, convert numbers to numbers
+    //Based on a stack overflow answer
+    const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
+    const rows = str.slice(str.indexOf("\n") + 1).split("\n").slice(0, -1); //Ignore the final \n that closes the file
+    // Map the rows
+    const arr = rows.map(row => {
+        const values = row.split(delimiter);
+        let object = {
+            gender: values[0], forecast_date: values[1], playin_flag: !!values[2],
+            rd_win: values.slice(3, 10).map(x => Number(x)),
+            results_to: Number(values[10]) - 1, team_alive: !!values[11],
+            team_id: Number(values[12]), team_name: values[13], team_rating: Number(values[14]), team_region: values[15], team_seed: values[16], team_slot: Number(values[17]),
+            short_team_name: "",
+        };
+        return object;
+    });
+    return arr; // return the array
+}
+async function load_file_json(filepath) {
+    //Load a file and convert to object using json
+    let response = await fetch(filepath);
+    let obj = await response.json();
+    return obj;
+}
+async function load_file_text(filepath) {
+    //Load a file and convert to text
+    let response = await fetch(filepath);
+    let text = await response.text();
+    return text;
+}
+function parse538csv(csv, teams) {
+}
+class Scenario2 {
+    constructor(probTable, teams) {
+    }
+}
+require(".//team_data.json");
+require(".//fivethirtyeight_ncaa_forecasts.csv");
+async function main2() {
+    //Load the teams data
+    let teams = await load_file_json(".//team_data.json");
+    console.log(teams);
+    //Load the primary csv File and convert to states
+    let text = await load_file_text(".//fivethirtyeight_ncaa_forecasts.csv"); //Testing
+    // let text: string = await load_file_text("https://projects.fivethirtyeight.com/march-madness-api/2023/fivethirtyeight_ncaa_forecasts.csv") //Production
+    let csv = csvToArray(text);
+    console.log(csv);
+}
+main2();
+//# sourceMappingURL=builder2.js.map
---
+++ out/scraper.js
@@ -0,0 +1,78 @@
+// ESPN Scraper
+const cheerio = require('cheerio');
+const fs = require('fs');
+const brackets = {
+    "Tyler": "https://fantasy.espn.com/tournament-challenge-bracket/2022/en/entry?entryID=63497066",
+    "Jordan": "https://fantasy.espn.com/tournament-challenge-bracket/2022/en/entry?entryID=67990401",
+    "Neel": "https://fantasy.espn.com/tournament-challenge-bracket/2022/en/entry?entryID=68360536",
+};
+async function main_scraper() {
+    let allBrackets = {};
+    let teams;
+    for (let player in brackets) {
+        let ret = await scrape_bracket(brackets[player]);
+        let selections = ret.selections;
+        allBrackets[player] = selections;
+        teams = ret.teams;
+        console.log(player, JSON.stringify(selections));
+    }
+    fs.writeFileSync("user_brackets_new.json", JSON.stringify(allBrackets));
+    fs.writeFileSync("team_data.json", JSON.stringify(teams));
+    console.log("Data write complete.");
+}
+async function scrape_bracket(url) {
+    // Pull the webpage and get the html
+    const resp = await fetch(url);
+    const responsetext = await resp.text();
+    //Load to jQuery similar file using cheerio
+    const $ = cheerio.load(responsetext);
+    //Pull data round by round
+    let roundNumber = 0;
+    let gamesPerRound = [32, 16, 8, 4, 2, 1];
+    let gameNumber = 0;
+    let gameInRoundNumber = 0;
+    let selections = [0];
+    let teams = {};
+    while (true) {
+        // Determine the game number and the round number: gameNumber, roundNumber
+        gameNumber += 1;
+        gameInRoundNumber += 1;
+        if (gameInRoundNumber > gamesPerRound[roundNumber]) {
+            // Move to the next round
+            roundNumber += 1;
+            gameInRoundNumber = 1;
+            // End after the finals
+            if (roundNumber >= gamesPerRound.length) {
+                break;
+            }
+        }
+        // Pull selection data
+        let matchup = $(`.matchup.m_${gameNumber}`);
+        for (let s = 1; s <= 2; s++) {
+            let slot = matchup.find(`.slot.s_${s}`);
+            let team = {
+                teamID: parseInt(slot.children().first().attr("data-id")),
+                sportsID: parseInt(slot.children().first().attr("data-sportsid")),
+                seed: parseInt(slot.find(".seed").text()),
+                abbreviation: slot.find(".abbrev").first().text(),
+                name: slot.find(".name").first().text(),
+            };
+            // Did player select team to advance
+            let selectedToAdvance = slot.find(".selectedToAdvance").length > 0;
+            if (selectedToAdvance) {
+                selections.push(team.teamID);
+            }
+            //In the first round, create data for all teams 
+            if (roundNumber === 0) {
+                teams[team.teamID] = team;
+            }
+        }
+    }
+    // Data pull loop complete return selections and teams
+    // console.log(selections)
+    // console.log(teams)
+    return { selections: selections, teams: teams };
+}
+main_scraper();
+//# sourceMappingURL=scraper.js.map
+//# sourceMappingURL=scraper.js.map
